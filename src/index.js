bc80b51d2ef6b861ac59be5f326ac09b505209e7
PauloChaser <pavel-kuznetsov93@mail.ru>
2023-04-27 12:42:55 +0300

    add layout

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./css/style.css";
+import {initModals} from "./js/modals";
+// import {initHeader} from "./js/header";
+
+
+document.addEventListener("DOMContentLoaded", function () {
+ initModals()
+ // initHeader()
+});
---
+++ src/js/header.js
@@ -0,0 +1,19 @@
+// import { Body } from './classes/Body';
+//
+// const menuButton = document.querySelector('.js-burger')
+// const headerMenu = document.querySelector('.js-header-menu')
+//
+// export function initHeader() {
+//     if (!isWindowSizeSmallerThen(1021)) {
+//         return
+//     }
+//     menuButton.addEventListener('click', function () {
+//         headerMenu.classList.toggle('header__menu--active')
+//         menuButton.classList.toggle('header__burger--active')
+//         Body.toggleBody()
+//     })
+// }
+//
+// function isWindowSizeSmallerThen(size = 1021) {
+//     return document.body.offsetWidth < size;
+// }
